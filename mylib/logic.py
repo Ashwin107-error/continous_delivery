2a838e78326d3df1e87150bed06393a3e331f12a
ashwin.nair <ashwin.nair@routemobile.com>
2024-09-02 12:14:21 +0530

    adding a new cli function

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,9 @@ def wiki(name="War goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia For Names"""
+
+    results = wikipedia.search(name)
+    return results
